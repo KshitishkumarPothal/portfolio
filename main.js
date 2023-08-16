7a1324d134ae1b6091113739b169e50561216296
Kshitish Kumar Pothal <106439854+KshitishkumarPothal@users.noreply.github.com>
2023-08-17 00:21:30 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+var typed = new Typed('.text', {
+    strings: ['Frontend developer' , 'web designer' , 'programmer'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
+// let menuToggle=document.querySelector('.toggle');
+// let menu=document.querySelector('.menu');
+// menuToggle.onclick=function(){
+//     menu.classList.toggle('active')
+//     menuToggle.classList.toggle('active')
+// }
+// constlist=document.querySelectorAll('li');
+// function activeLink(){
+//     list.forEach((item) => {
+//         item.classList.remove('active')
+        
+//     });
+//     this. classList.add('active')
+// }
+// list.forEach((item)=>
+// item.addEventListner('click',activeLink))
